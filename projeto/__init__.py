369e08ccbb07ca0d1e94ce25666bdac11564e2d3
Miguelsoares23 <miguelcristiano309@gmail.com>
2025-07-09 14:56:24 -0300

    bd com menos seguraça

--- projeto/__init__.py
+++ projeto/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import FlaskSQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from projeto.views import homepage
+from projeto.models import Teste
--- projeto/views.py
+++ projeto/views.py
@@ -1,10 +1,32 @@
-from projeto import app
-from flask import render_template, url_for
+from projeto import app, db
+from flask import render_template, url_for, request
+
+from projeto.models import Teste
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
 
-@app.route('/teste')
+@app.route('/teste', methods=['GET', "POST"])
 def testepage():
-    return "oi 2.0"
+    context = {}
+    if request.method == "GET":
+        pesquisa = request.args.get('pesquisa')
+        print('GET:', pesquisa)
+        context.update({'pesquisa': pesquisa})
+    if request.method == "POST":
+        nome = request.form['nome']
+        email = request.form['email']
+        assunto = request.form['assunto']
+        mensagem = request.form['mensagem']
+        
+        teste = Teste(
+            nome=nome,
+            email=email,
+            assunto=assunto,
+            mensagem=mensagem
+        )
+        db.session.add(teste)
+        db.session.commit()
+
+    return render_template('teste.html', context=context)
