2438c21e811e21400253cca18e66d2ab1b5165e3
MiguelSoares23 <miguelcristiano309@gmail.com>
2025-07-14 14:28:45 -0300

    form part 2 concluida

--- projeto/forms.py
+++ projeto/forms.py
@@ -2,9 +2,23 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Email
 
+from projeto import db
+from projeto.models import Teste
+
 class TesteForm(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired()])
     email = StringField('E-Mail', validators=[DataRequired(), Email()])
     assunto = StringField('Assunto', validators=[DataRequired()])
     mensagem = StringField('Mensagem', validators=[DataRequired()])
     btnSubmit = SubmitField('Enviar')
+
+    def save(self):
+        teste = Teste(
+            nome = self.nome.data,
+            email = self.email.data,
+            assunto = self.assunto.data,
+            mensagem = self.mensagem.data
+        )
+
+        db.session.add(teste)
+        db.session.commit()
--- projeto/views.py
+++ projeto/views.py
@@ -1,5 +1,5 @@
 from projeto import app, db
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, redirect
 
 from projeto.models import Teste
 from projeto.forms import TesteForm
@@ -9,24 +9,13 @@ def homepage():
     return render_template('index.html')
 
 
-@app.route('/teste_old', methods=['GET', "POST"])
+@app.route('/teste', methods=['GET', "POST"])
 def teste():
     form = TesteForm()
     context = {}
-    if request.method == "POST":
-        nome = request.form['nome']
-        email = request.form['email']
-        assunto = request.form['assunto']
-        mensagem = request.form['mensagem']
-        
-        teste = Teste(
-            nome=nome,
-            email=email,
-            assunto=assunto,
-            mensagem=mensagem
-        )
-        db.session.add(teste)
-        db.session.commit()
+    if form.validate_on_submit():
+        form.save()
+        return redirect(url_for('homepage'))
 
     return render_template('teste.html', context=context,  form=form)
 
